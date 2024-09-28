51083fe2439e0f6e85decbf00b8f24d22e257b06
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2024-09-28 21:33:36 +0900

    [Silver IV] Title: 스택, Time: 384 ms, Memory: 112828 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+arr = []
+for i in range(n):
+    command = input().split()
+
+    if (command[0] == 'push'): arr.append(command[1])
+    elif (command[0] == 'pop'):
+        if (len(arr) == 0): print(-1)
+        else: print(arr.pop())
+    elif (command[0] == 'size'): print(len(arr))
+    elif (command[0] == 'empty'):
+        if (len(arr) == 0): print(1)
+        else: print(0)
+    elif (command[0] == 'top'):
+        if (len(arr) == 0): print(-1)
+        else: print(arr[-1])
