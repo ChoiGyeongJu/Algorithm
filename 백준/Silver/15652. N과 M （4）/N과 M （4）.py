7925c2a7d699630d0ab310c44fac47b028167831
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2024-10-12 18:48:58 +0900

    [Silver III] Title: N과 M (4), Time: 140 ms, Memory: 113332 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = []
+def dfs(num):
+    if len(arr) == m:
+        print(*arr)
+        return
+    
+    for i in range(num, n + 1):
+        arr.append(i)
+        dfs(i)
+        arr.pop()
+
+dfs(1)
