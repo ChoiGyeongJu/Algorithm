0871fc3f6a032e19dc00e52170504702abff2fab
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2024-10-13 18:05:05 +0900

    [level 2] Title: 프로세스, Time: 1.31 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    queue = deque([])
+    for i in range(len(priorities)):
+        queue.append((priorities[i], i))
+
+    while queue:
+        prior, index = queue.popleft()
+        if queue and max(queue)[0] > prior:
+            queue.append((prior, index))
+        else:
+            answer += 1
+            if index == location:
+                break
+    return answer
+
+solution([2, 1, 3, 2], 2)
