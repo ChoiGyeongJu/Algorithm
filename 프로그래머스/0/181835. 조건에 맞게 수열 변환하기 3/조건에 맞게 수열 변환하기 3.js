9866a185e68687acf54d97be7137a3017314f671
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2024-10-21 19:13:29 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 18.55 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -1,15 +1,6 @@
 function solution(arr, k) {
-    let answer = []
-    if (k % 2 === 0) {
-        // k 더하기
-        for (let i = 0; i < arr.length; i++) {
-            answer.push(arr[i] + k)
-        }
-    } else {
-        // k 곱하기
-        for (let i = 0; i < arr.length; i++) {
-            answer.push(arr[i] * k)
-        }
-    }
-    return answer
+    return arr.map(v => {
+        if (k % 2 === 0) return v + k
+        else return v * k
+    })
 }
