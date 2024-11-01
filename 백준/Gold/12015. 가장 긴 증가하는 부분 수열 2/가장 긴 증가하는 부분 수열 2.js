d24290bfd1ca648d22558966bf93c9a28cef38e1
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2024-11-01 19:08:27 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 728 ms, Memory: 149264 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.js
@@ -0,0 +1,46 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let lines = []
+
+rl.on("line", (input) => {
+    lines.push(input.trim())
+    
+    if (lines.length === 2) {
+        rl.close()
+    }
+});
+
+rl.on("close", () => {
+    const n = parseInt(lines.shift())
+    const arr = lines[0].split(' ').map(Number)
+
+    let result = [0]
+    for (const num of arr) {
+        if (result[result.length - 1] < num) {
+            result.push(num)
+        } else {
+            result[bin_search(num)] = num
+        }
+    }
+
+    function bin_search(v) {
+        let start = 0, end = result.length
+        while (start <= end) {
+            let mid = Math.floor((start + end) / 2)
+            
+            if (result[mid] < v) {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+        return start
+    }
+
+    console.log(result.length - 1)
+});
