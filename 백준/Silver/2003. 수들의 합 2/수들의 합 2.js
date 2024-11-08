9d9f829069b48db51fe3412eeb969578a9eb58f2
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2024-11-08 13:05:52 +0900

    [Silver IV] Title: 수들의 합 2, Time: 228 ms, Memory: 10268 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
@@ -1,38 +1,33 @@
-const readline = require("readline");
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
-let lines = []
+let lines = [];
 
-rl.on("line", (input) => {
-    lines.push(input.trim())
-    
-    if (lines.length === 2) {
-        rl.close()
-    }
+rl.on('line', input => {
+  lines.push(input.trim());
+  if (lines.length === 2) {
+    rl.close();
+  }
 });
 
-rl.on("close", () => {
-    const [n, m] = lines.shift().split(' ').map(Number)
-    const arr = lines[0].split(' ').map(Number)
-
-    let answer = 0
-    function dfs(start, value) {
-        if (start === n) return
-
-        if (arr[start] + value === m) answer += 1
-        else if (arr[start] + value < m) {
-            dfs(start + 1, arr[start] + value)
-        } else {
-            return
-        }
+rl.on('close', () => {
+  const [n, m] = lines[0].split(' ').map(Number)
+  const arr = lines[1].split(' ').map(Number)
+  
+  // 연속된 수들의 합이 m이 되는 경우의 수
+  let answer = 0
+  for (let i = 0; i < n; i++) {
+    let tmp = 0
+    for (let j = i; j < n; j++) {
+      tmp += arr[j]
+      if (tmp === m) answer += 1
+      else if (tmp > m) break
     }
+  }
 
-    for (let i = 0; i < n; i++) {
-        dfs(i, 0)
-    }
-    console.log(answer)
+  console.log(answer)
 });
