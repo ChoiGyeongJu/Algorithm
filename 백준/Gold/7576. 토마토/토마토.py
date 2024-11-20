de8b03a6366d44f36b53b6622f4564a5ae27503c
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2024-11-20 14:45:37 +0900

    [Gold V] Title: 토마토, Time: 320 ms, Memory: 156548 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,52 +1,34 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 m, n = map(int, input().split())
-graph = []
-visited = [[False for _ in range(m)] for _ in range(n)]
-for i in range(n):
-    arr = list(map(int, input().split()))
-    graph.append(arr)
+grid = []
+for _ in range(n):
+    grid.append(list(map(int, input().split())))
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 queue = deque([])
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 1:
-            queue.append((i, j, 0))
-            visited[i][j] = True
+        if grid[i][j] == 1:
+            queue.append((i, j))
 
-day = 0
-
-# 익은 토마토는 1, 익지 않은 토마토는 0, 비어있는 칸은 -1
 while queue:
-    x, y, day = queue.popleft()
-    
-    if x < n - 1 and graph[x + 1][y] != -1 and visited[x + 1][y] == False:
-        queue.append((x+1, y, day + 1))
-        graph[x + 1][y] = 1
-        visited[x + 1][y] = True
+    x, y = queue.popleft()
+    for i in range(4):
+        nx, ny = dx[i] + x, dy[i] + y
+        if 0 <= nx < n and 0 <= ny < m:
+            if grid[nx][ny] == 0:
+                grid[nx][ny] = grid[x][y] + 1
+                queue.append((nx, ny))
 
-    if y < m - 1 and graph[x][y + 1] != -1 and visited[x][y + 1] == False:
-        queue.append((x, y + 1, day + 1))
-        graph[x][y + 1] = 1
-        visited[x][y + 1] = True
-    
-    if x > 0 and graph[x - 1][y] != -1 and visited[x - 1][y] == False:
-        queue.append((x - 1, y, day + 1))
-        graph[x - 1][y] = 1
-        visited[x - 1][y] = True
-    
-    if y > 0 and graph[x][y - 1] != -1 and visited[x][y - 1] == False:
-        queue.append((x, y - 1, day + 1))
-        graph[x][y - 1] = 1
-        visited[x][y - 1] = True
-
-flag = True
+ans = 0
 for i in range(n):
-    if 0 in graph[i]:
-        flag = False
-        break
-
-if flag:
-    print(day)
-else:
-    print(-1)
+    if 0 in grid[i]:
+        print(-1)
+        exit()
+    ans = max(max(grid[i]), ans)
+print(ans - 1)
