aea35ec06edad62c37504381944acaab9c08295b
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2024-12-11 17:24:25 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,11 +1,19 @@
-SELECT 
-    ID, 
-    EMAIL, 
-    FIRST_NAME, 
-    LAST_NAME
-FROM 
-    DEVELOPERS
-WHERE 
-    SKILL_CODE & (256 + 1024) > 0
-ORDER BY 
-    ID ASC;
+SELECT DISTINCT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
+FROM DEVELOPERS D 
+JOIN SKILLCODES S 
+ON S.NAME IN ('Python', 'C#')
+AND D.SKILL_CODE & S.CODE = S.CODE
+ORDER BY 1
+
+# SELECT
+#     DISTINCT id,
+#     email,
+#     first_name,
+#     last_name
+# FROM
+#     developers d
+#     JOIN skillcodes s
+#     ON s.name IN ('C#', 'Python')
+#     AND d.skill_code & s.code = s.code
+# ORDER BY
+#     1
