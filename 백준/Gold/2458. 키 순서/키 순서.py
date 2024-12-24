3a815f08559cdc338ea17e9d13e3176e686aad7c
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2024-12-24 21:03:23 +0900

    [Gold IV] Title: 키 순서, Time: 1780 ms, Memory: 117404 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+
+graph = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if graph[i][j] == 1 or (graph[i][k] == 1 and graph[k][j] == 1):
+                graph[i][j] = 1
+
+answer = 0
+for i in range(1, n + 1):
+    cnt = 0
+    for j in range(1, n + 1):
+        cnt += graph[i][j] + graph[j][i]
+
+    if cnt == n - 1:
+        answer += 1
+
+print(answer)
