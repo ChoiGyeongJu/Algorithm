50b7f983c344dfd8f547f17afd2d639b9b0448a3
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2025-01-10 19:01:18 +0900

    [level 2] Title: 숫자의 표현, Time: 2.42 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,18 +1,17 @@
 function solution(n) {
-    var answer = 0;
+    let answer = 0;
     
     for (let i = 1; i <= n; i++) {
-        let sum = 0;
-        
+        let acc = 0;
         for (let j = i; j <= n; j++) {
-            sum += j;
-            if (sum == n) {
+            acc += j;
+            if (acc === n) {
                 answer += 1;
-            } else if (sum > n) {
+                
+            } else if (acc > n) {
                 break;
             }
         }
     }
-    
     return answer;
 }
