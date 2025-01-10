5f35339835ffea7e9362925b6bc7ca028cdca325
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2025-01-10 18:13:06 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,8 +1,5 @@
 function solution(s) {
     let arr = s.split(' ')
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = Number(arr[i])
-    }
-    arr.sort((a, b) => a - b)
-    return String(arr[0]) + ' ' + String(arr[arr.length - 1])
+    
+    return Math.min(...arr) + ' ' + Math.max(...arr)
 }
