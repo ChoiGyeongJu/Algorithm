942d22b1a8ab8208dc23f741dff71efdcad5a705
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2025-01-23 16:29:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1212 ms, Memory: 101176 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -0,0 +1,42 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let lines = [];
+
+rl.on("line", (input) => {
+  lines.push(input.trim());
+  if (lines.length === Number(lines[0].split(" ")[1]) + 1) {
+    rl.close();
+  }
+});
+
+rl.on("close", () => {
+  const [n, m] = lines[0].split(" ").map(Number);
+  const grid = Array.from({ length: n + 1 }, () => []);
+  for (let i = 1; i < m + 1; i++) {
+    const [x, y] = lines[i].split(" ").map(Number);
+    grid[x].push(y);
+    grid[y].push(x);
+  }
+
+  const visited = new Set();
+  function dfs(node) {
+    visited.add(node);
+    for (const next of grid[node]) {
+      if (!visited.has(next)) dfs(next);
+    }
+  }
+
+  let answer = 0;
+  for (let i = 1; i < n + 1; i++) {
+    if (!visited.has(i)) {
+      dfs(i);
+      answer++;
+    }
+  }
+  console.log(answer);
+});
