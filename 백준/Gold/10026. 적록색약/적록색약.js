d6759c8aec3370c5e80de89ffd0a8a633d035e3b
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2025-01-24 13:12:38 +0900

    [Gold V] Title: 적록색약, Time: 192 ms, Memory: 12964 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.js
+++ 백준/Gold/10026. 적록색약/적록색약.js
@@ -5,80 +5,72 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let lines = []
+let lines = [];
 
 rl.on("line", (input) => {
-    lines.push(input.trim())
-    
-    if (lines.length === parseInt(lines[0]) + 1) {
-        rl.close()
-    }
+  lines.push(input.trim());
+  if (lines.length === Number(lines[0]) + 1) {
+    rl.close();
+  }
 });
 
 rl.on("close", () => {
-    const n = parseInt(lines.shift())
-    let grid = []
-    for (let i = 0; i < n; i++) {
-        grid.push(lines[i].split(''))
+  const n = Number(lines[0]);
+  const normal = lines.slice(1).map((v) => v.split(""));
+  const copy = JSON.parse(JSON.stringify(normal));
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (copy[i][j] === "R") copy[i][j] = "G";
     }
-    let weak = Array.from({ length: n }, () => Array(n).fill(0))
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === 'B') {
-                weak[i][j] = 'B'
-            } else {
-                weak[i][j] = 'R'
-            }
+  }
+
+  const dx = [0, 0, -1, 1];
+  const dy = [1, -1, 0, 0];
+
+  function bfs(grid, x, y, color) {
+    const q = [];
+    q.push([x, y]);
+    grid[x][y] = "X";
+    let head = 0;
+    while (q.length > head) {
+      const [x, y] = q[head++];
+
+      for (let i = 0; i < 4; i++) {
+        const nx = dx[i] + x,
+          ny = dy[i] + y;
+        if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+          if (grid[nx][ny] === color) {
+            grid[nx][ny] = "X";
+            q.push([nx, ny]);
+          }
         }
+      }
     }
+  }
 
-    const dx = [0, 0, 1, -1]
-    const dy = [1, -1, 0, 0]
-    function bfs(x, y, isWeak) {
-        let queue = [[x, y]]
-        const current = isWeak ? weak[x][y] : grid[x][y]
-        if (isWeak) {
-            weak[x][y] = 0
-        } else {
-            grid[x][y] = 0
-        }
+  let a = 0,
+    b = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (normal[i][j] === "R") {
+        a++;
+        bfs(normal, i, j, "R");
+      } else if (normal[i][j] === "G") {
+        a++;
+        bfs(normal, i, j, "G");
+      } else if (normal[i][j] === "B") {
+        a++;
+        bfs(normal, i, j, "B");
+      }
 
-        while (queue.length > 0) {
-            const [x, y] = queue.shift()
-            for (let i = 0; i < 4; i++) {
-                const nx = dx[i] + x
-                const ny = dy[i] + y
-
-                if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-                    if (isWeak) {
-                        if (weak[nx][ny] === current) {
-                            queue.push([nx, ny])
-                            weak[nx][ny] = 0
-                        }
-                    } else {
-                        if (grid[nx][ny] === current) {
-                            queue.push([nx, ny])
-                            grid[nx][ny] = 0
-                        }
-                    }
-                }
-            }
-        }
+      if (copy[i][j] === "G") {
+        b++;
+        bfs(copy, i, j, "G");
+      } else if (copy[i][j] === "B") {
+        b++;
+        bfs(copy, i, j, "B");
+      }
     }
-
-    let a = 0
-    let b = 0
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] !== 0) {
-                bfs(i, j, false)
-                a += 1
-            }
-            if (weak[i][j] !== 0) {
-                bfs(i, j, true)
-                b += 1
-            }
-        }
-    }
-    console.log(a, b)
+  }
+  console.log(a, b);
 });
