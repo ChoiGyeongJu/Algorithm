669e019549d6be2b0f290aee2e5e6d699aa512c8
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2025-01-25 14:58:21 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1804 ms, Memory: 310500 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -1,61 +1,62 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 let lines = [];
 
-rl.on('line', input => {
+rl.on("line", (input) => {
   lines.push(input.trim());
-  if (lines.length === parseInt(lines[0].split(' ')[0]) + 1) {
+  if (lines.length === Number(lines[0].split(" ")[0]) + 1) {
     rl.close();
   }
 });
 
-rl.on('close', () => {
-  let idx = 0;
-  const [n, m] = lines[idx++].split(' ').map(Number);
-  let grid = [];
-  for (let i = 0; i < n; i++) {
-    grid.push(lines[idx++].split(''));
-  }
+rl.on("close", () => {
+  const [n, m] = lines[0].split(" ").map(Number);
+  const grid = lines.splice(1).map((v) => v.split("").map(Number));
 
-  let visited = Array.from({ length: n }, () => Array.from({ length: m }, () => [false, false]));
-  visited[0][0][0] = true;
-  
-  let queue = [];
-  let front = 0;  // 큐에서 요소를 꺼낼 인덱스
-  queue.push([0, 0, 1, 0]); // x, y, 거리, 벽 부순 횟수
+  // 1은 벽, 한개까지 부술 수 있음, 최단거리 출력
+  function bfs() {
+    let head = 0;
+    const q = [];
+    q.push([0, 0, 1, 0]);
 
-  const dx = [0, 0, 1, -1];
-  const dy = [1, -1, 0, 0];
-  let answer = -1;
+    const visited = Array.from({ length: n }, () =>
+      Array.from({ length: m }, () => [false, false])
+    );
+    visited[0][0][0] = true;
 
-  while (front < queue.length) {
-    const [x, y, dist, cnt] = queue[front++];
-    
-    if (x === n - 1 && y === m - 1) {
-      answer = dist;
-      break;
-    }
+    const dx = [0, 0, 1, -1];
+    const dy = [1, -1, 0, 0];
 
-    for (let i = 0; i < 4; i++) {
-      const nx = dx[i] + x, ny = dy[i] + y;
-
-      if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-        if (!visited[nx][ny][cnt] && grid[nx][ny] === '0') {
-          visited[nx][ny][cnt] = true;
-          queue.push([nx, ny, dist + 1, cnt]);
-        }
-        else if (cnt === 0 && !visited[nx][ny][cnt + 1] && grid[nx][ny] === '1') {
-          visited[nx][ny][cnt + 1] = true;
-          queue.push([nx, ny, dist + 1, cnt + 1]);
+    while (q.length > head) {
+      const [x, y, dist, cnt] = q[head++];
+      if (x === n - 1 && y === m - 1) {
+        return dist;
+      }
+      for (let i = 0; i < 4; i++) {
+        const nx = dx[i] + x;
+        const ny = dy[i] + y;
+        if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+          if (grid[nx][ny] === 0 && !visited[nx][ny][cnt]) {
+            visited[nx][ny][cnt] = true;
+            q.push([nx, ny, dist + 1, cnt]);
+          } else if (
+            grid[nx][ny] === 1 &&
+            cnt === 0 &&
+            !visited[nx][ny][cnt + 1]
+          ) {
+            visited[nx][ny][cnt + 1] = true;
+            q.push([nx, ny, dist + 1, cnt + 1]);
+          }
         }
       }
     }
+    return -1;
   }
 
-  console.log(answer);
+  console.log(bfs());
 });
