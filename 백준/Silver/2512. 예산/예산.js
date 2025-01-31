e6b18254f20ae49db0fa43f4e76eb0a2153c29af
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2025-01-31 14:29:08 +0900

    [Silver II] Title: 예산, Time: 180 ms, Memory: 12372 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.js
+++ 백준/Silver/2512. 예산/예산.js
@@ -20,22 +20,20 @@ rl.on("close", () => {
   const m = Number(lines[2]);
 
   let start = 1;
-  let end = m;
+  let end = Math.max(...arr);
   let answer = 0;
   while (start <= end) {
     const mid = Math.floor((start + end) / 2);
 
-    const val = [];
     let total = 0;
     for (const x of arr) {
-      val.push(Math.min(x, mid));
       total += Math.min(x, mid);
     }
 
     if (total > m) {
       end = mid - 1;
     } else {
-      answer = Math.max(answer, Math.max(...val));
+      answer = Math.max(answer, mid);
       start = mid + 1;
     }
   }
