3e88fe54953257c605d7b688d56283bf2dfcd061
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2025-02-03 13:31:18 +0900

    [Gold IV] Title: 이분 그래프, Time: 1800 ms, Memory: 152796 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.js
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.js
@@ -5,46 +5,55 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let lines = []
+let lines = [];
 
 rl.on("line", (input) => {
-    lines.push(input.trim())
+  lines.push(input.trim());
 });
 
 rl.on("close", () => {
-    const k = parseInt(lines.shift())
-    let idx = 0
-    for (let t = 0; t < k; t++) {
-        const [V, E] = lines[idx++].split(' ').map(Number)
-        let graph = Array.from({ length: V + 1 }, () => Array())
-        for (let i = 0; i < E; i++) {
-            const [x, y] = lines[idx++].split(' ').map(Number)
-            graph[x].push(y)
-            graph[y].push(x)
-        }
-        
-        let visited = new Array(V + 1).fill(0)
-        function solution() {
-            for (let i = 1; i < V + 1; i++) {
-                if (visited[i] === 0) {
-                    visited[i] = 1
-                    let queue = [i]
-                    while (queue.length > 0) {
-                        const node = queue.shift()
-                        for (const next of graph[node]) {
-                            if (visited[next] === 0) {
-                                visited[next] = -1 * visited[node]
-                                queue.push(next)
-                            } else if (visited[next] === visited[node]) {
-                                return false
-                            }
-                        }
-                    }
-                }
-            }
-            return true
-        }
-        if (solution()) console.log('YES')
-        else console.log('NO')
+  let idx = 0;
+  const K = Number(lines[idx++]);
+
+  for (let i = 0; i < K; i++) {
+    const [V, E] = lines[idx++].split(" ").map(Number);
+    const grid = Array.from({ length: V + 1 }, () => []);
+
+    for (let j = 0; j < E; j++) {
+      const [u, v] = lines[idx++].split(" ").map(Number);
+      grid[u].push(v);
+      grid[v].push(u);
     }
+
+    const colors = new Array(V + 1).fill(0);
+    function dfs(node) {
+      colors[node] = 1;
+      const stack = [];
+      stack.push(node);
+
+      while (stack.length) {
+        const x = stack.pop();
+        for (const next of grid[x]) {
+          if (colors[next] === 0) {
+            colors[next] = -colors[x];
+            stack.push(next);
+          } else if (colors[next] === colors[x]) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    let flag = true;
+    for (let k = 1; k <= V; k++) {
+      if (colors[k] === 0) {
+        if (!dfs(k)) {
+          flag = false;
+          break;
+        }
+      }
+    }
+    console.log(flag ? "YES" : "NO");
+  }
 });
