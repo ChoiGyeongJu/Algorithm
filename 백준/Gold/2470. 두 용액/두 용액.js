c9b443c5147e4c681178ac1eb70ce19e5ec7cc99
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2025-02-03 17:21:50 +0900

    [Gold V] Title: 두 용액, Time: 224 ms, Memory: 27832 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.js
+++ 백준/Gold/2470. 두 용액/두 용액.js
@@ -9,34 +9,27 @@ let lines = [];
 
 rl.on("line", (input) => {
   lines.push(input.trim());
-  if (lines.length === 2) {
-    rl.close();
-  }
 });
 
 rl.on("close", () => {
   const n = Number(lines[0]);
   const arr = lines[1].split(" ").map(Number);
   arr.sort((a, b) => a - b);
-
   let start = 0;
   let end = n - 1;
 
   let answer = [arr[start], arr[end]];
-  let tmp = Math.abs(arr[start] + arr[end]);
-
   while (start < end) {
     const val = arr[start] + arr[end];
 
-    if (Math.abs(val) < tmp) {
-      tmp = Math.abs(val);
+    if (Math.abs(val) < Math.abs(answer[0] + answer[1])) {
       answer = [arr[start], arr[end]];
     }
 
     if (val < 0) {
-      start += 1;
+      start++;
     } else {
-      end -= 1;
+      end--;
     }
   }
 
