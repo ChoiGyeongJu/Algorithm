eb4f3fe2ab1fea228a043c5c05dbad20db319caa
최경주 <97934158+ChoiGyeongJu@users.noreply.github.com>
2025-02-03 16:27:21 +0900

    [Silver IV] Title: 수들의 합 2, Time: 164 ms, Memory: 10004 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
@@ -1,33 +1,39 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 let lines = [];
 
-rl.on('line', input => {
+rl.on("line", (input) => {
   lines.push(input.trim());
-  if (lines.length === 2) {
-    rl.close();
-  }
 });
 
-rl.on('close', () => {
-  const [n, m] = lines[0].split(' ').map(Number)
-  const arr = lines[1].split(' ').map(Number)
-  
-  // 연속된 수들의 합이 m이 되는 경우의 수
-  let answer = 0
-  for (let i = 0; i < n; i++) {
-    let tmp = 0
-    for (let j = i; j < n; j++) {
-      tmp += arr[j]
-      if (tmp === m) answer += 1
-      else if (tmp > m) break
+rl.on("close", () => {
+  const [n, m] = lines[0].split(" ").map(Number);
+  const arr = lines[1].split(" ").map(Number);
+
+  // 연속된 구간의 합이 m이 되는 경우의 수
+  let start = 0,
+    end = 0,
+    sum = 0,
+    cnt = 0;
+
+  while (end < n) {
+    sum += arr[end];
+
+    while (sum > m && start <= end) {
+      sum -= arr[start];
+      start++;
     }
+
+    if (sum === m) {
+      cnt++;
+    }
+    end++;
   }
 
-  console.log(answer)
+  console.log(cnt);
 });
